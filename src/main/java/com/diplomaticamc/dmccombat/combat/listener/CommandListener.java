9e3c5e549be939b00a717753eab5ce3fce2c6393
Tristan <218382355+Tristan-ye@users.noreply.github.com>
2025-08-08 13:46:20 +0100

    Update CommandListener.java

--- src/main/java/com/diplomaticamc/dmccombat/combat/listener/CommandListener.java
+++ src/main/java/com/diplomaticamc/dmccombat/combat/listener/CommandListener.java
@@ -1,18 +1,31 @@
 package com.diplomaticamc.dmccombat.combat.listener;
-import com.google.common.collect.ImmutableSet;
 import com.diplomaticamc.dmccombat.combat.CombatHandler;
+import com.diplomaticamc.dmccombat.DMCCombat;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 
-import java.util.Set;
+import java.util.List;
 
 public class CommandListener implements Listener {
 
-    private static final Set<String> BLACKLISTED_COMMANDS = ImmutableSet.of(
-            "t spawn", "n spawn", "warp", "trade", "res spawn", "home", "tradesystem:trade","town spawn","nation spawn","resident spawn","homes","towny:nation spawn","towny:town spawn","towny:resident spawn","player spawn","towny:player spawn","towny:n spawn","towny:nat spawn","towny:tw spawn","towny:res spawn","towny:t spawn","suicide");
+    private List<String> blockedCommands;
+
+    public CommandListener(DMCCombat plugin) {
+        this.blockedCommands = plugin.getConfig().getStringList("blocked-commands")
+                .stream()
+                .map(String::toLowerCase)
+                .toList();
+    }
+
+    public void loadBlockedCommands(DMCCombat plugin) {
+        this.blockedCommands = plugin.getConfig().getStringList("blocked-commands")
+                .stream()
+                .map(String::toLowerCase)
+                .toList();
+    }
 
     @EventHandler
     public void onPreProcessCommand(PlayerCommandPreprocessEvent event) {
@@ -20,11 +33,11 @@ public class CommandListener implements Listener {
 
         // Check if the player is in combat and doesn't have bypass permission
         if (CombatHandler.isTagged(player) && !player.hasPermission("dmccombat.combattag.bypass")) {
-            String message = event.getMessage().substring(1); // Remove leading "/"
+            String message = event.getMessage().substring(1).toLowerCase(); // Remove leading "/"
 
             // Check if the command is blacklisted
-            for (String blacklistedCommand : BLACKLISTED_COMMANDS) {
-                if (message.equalsIgnoreCase(blacklistedCommand) || message.toLowerCase().startsWith(blacklistedCommand + " ")) {
+            for (String blacklistedCommand : blockedCommands) {
+                if (message.equals(blacklistedCommand) || message.startsWith(blacklistedCommand + " ")) {
                     event.setCancelled(true);
                     player.sendMessage(ChatColor.RED + "You can't use that command while being in combat.");
                     return;
